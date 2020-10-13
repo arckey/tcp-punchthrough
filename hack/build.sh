adc86656aa2090e3d094e68ebc7cb2dd8cd10f6b
Roi Kramer <roi.kramer@codefresh.io>
2020-10-13 20:16:41 +0300

    wip

--- hack/build.sh
+++ hack/build.sh
@@ -1,10 +1,3 @@
-# build client
-# go build -v -o ./client/client ./client/.
-
-# build server
-# go build -v -o ./server/server ./server/.
-# compile flatbuffers
-flatc -o types -I flatbuffers --go flatbuffers/*.fbs
 
 # build peer
 go build -v -o ./peer/peer ./peer/.
